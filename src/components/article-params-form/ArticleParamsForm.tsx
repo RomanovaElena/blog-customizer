d1da380b3fdb2c5871de05a475ac0ffc21d7c2b0
Elena Romanova <romanova.bim@gmail.com>
2025-06-05 18:23:37 +0500

    реализовать механику открытия-закрытия панели с формой

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,27 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	// Состояние формы (открыта/закрыта)
+	const [isOpen, setIsOpen] = useState(false);
+	// Обработчик клика на кнопку стрелки
+	const toggleIsOpen = () => {
+		setIsOpen(!isOpen);
+	};
+
+	// Условное применение классов при открытии формы
+	const formStyle = clsx({
+		[styles.container]: true,
+		[styles.container_open]: isOpen,
+	});
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={toggleIsOpen} />
+			<aside className={formStyle}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
